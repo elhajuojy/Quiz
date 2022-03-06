6245fa29898cccfe53cfb3e29795ba0954285281
elhajuojy <souirimehdi311@gmail.com>
2022-03-06 22:37:55 +0100

    complete form

--- Quiz/Form1.Designer.cs
+++ Quiz/Form1.Designer.cs
@@ -29,12 +29,11 @@ namespace Quiz
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textSearch = new System.Windows.Forms.TextBox();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.button17 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -48,37 +47,27 @@ namespace Quiz
             this.label1.TabIndex = 0;
             this.label1.Text = "Quiz";
             // 
-            // textBox1
+            // textSearch
             // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Font = new System.Drawing.Font("Poppins SemiBold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.ForeColor = System.Drawing.Color.LightSalmon;
-            this.textBox1.Location = new System.Drawing.Point(285, 103);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(554, 44);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "Search for Quiz";
+            this.textSearch.BackColor = System.Drawing.SystemColors.Control;
+            this.textSearch.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textSearch.Font = new System.Drawing.Font("Poppins SemiBold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textSearch.ForeColor = System.Drawing.Color.LightSalmon;
+            this.textSearch.Location = new System.Drawing.Point(285, 103);
+            this.textSearch.Multiline = true;
+            this.textSearch.Name = "textSearch";
+            this.textSearch.Size = new System.Drawing.Size(554, 44);
+            this.textSearch.TabIndex = 1;
+            this.textSearch.Text = "Search for Quiz";
+            this.textSearch.TextChanged += new System.EventHandler(this.textSearch_TextChanged);
+            this.textSearch.Enter += new System.EventHandler(this.textBox1_Enter);
             // 
             // flowLayoutPanel1
             // 
-            this.flowLayoutPanel1.Controls.Add(this.button17);
             this.flowLayoutPanel1.Location = new System.Drawing.Point(44, 227);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(953, 233);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(953, 320);
             this.flowLayoutPanel1.TabIndex = 2;
-            // 
-            // button17
-            // 
-            this.button17.Font = new System.Drawing.Font("Poppins SemiBold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button17.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.button17.Location = new System.Drawing.Point(3, 3);
-            this.button17.Name = "button17";
-            this.button17.Size = new System.Drawing.Size(184, 52);
-            this.button17.TabIndex = 42;
-            this.button17.Text = "PHP";
-            this.button17.UseVisualStyleBackColor = true;
             // 
             // panel1
             // 
@@ -92,14 +81,15 @@ namespace Quiz
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1066, 549);
+            this.ClientSize = new System.Drawing.Size(1066, 591);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.flowLayoutPanel1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textSearch);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.flowLayoutPanel1.ResumeLayout(false);
+            this.Text = "Quizo form";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -108,9 +98,8 @@ namespace Quiz
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textSearch;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
-        private System.Windows.Forms.Button button17;
         private System.Windows.Forms.Panel panel1;
     }
 }
--- Quiz/Form1.cs
+++ Quiz/Form1.cs
@@ -16,5 +16,48 @@ namespace Quiz
         {
             InitializeComponent();
         }
+        QuizEntities db = new QuizEntities();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ActiveControl = label1;
+            var lanlist = db.langs.Select(c => new { c.name_Lang });
+
+            foreach(var l in lanlist)
+            {
+                Button btn = new Button();
+                btn.Text = l.name_Lang;
+                btn.ForeColor = Color.BlueViolet;
+                btn.Size = new Size(150,45);
+                btn.Font = new Font("Poppins", 12,FontStyle.Bold);
+                flowLayoutPanel1.Controls.Add(btn);
+
+            }
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            textSearch.Text = string.Empty;
+        }
+
+        private void textSearch_TextChanged(object sender, EventArgs e)
+        {
+            flowLayoutPanel1.Controls.Clear();
+
+            var Query = db.langs.Where(lan => lan.name_Lang.Contains(textSearch.Text));
+
+            foreach (var l in Query)
+            {
+                Button btn = new Button();
+                btn.Text = l.name_Lang;
+                btn.ForeColor = Color.BlueViolet;
+                btn.Size = new Size(150, 45);
+                btn.Font = new Font("Poppins", 12, FontStyle.Bold);
+                flowLayoutPanel1.Controls.Add(btn);
+
+            }
+
+
+        }
     }
 }
